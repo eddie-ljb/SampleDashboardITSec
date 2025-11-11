65a23ee5ce2df01dc0928252ad4e9e56c16467ff
eddie-ljb <69401607+eddie-ljb@users.noreply.github.com>
2025-11-11 11:03:39 +0100

    docker compose

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgxGoogleAnalyticsModule } from '@hakimio/ngx-google-analytics';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterModule],
+    imports: [RouterModule, NgxGoogleAnalyticsModule],
     template: `
         <router-outlet></router-outlet>`
 })
