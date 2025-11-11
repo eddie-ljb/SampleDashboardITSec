8ca54d33f665d7c8c8da91012e63132565493a6b
eddie-ljb <69401607+eddie-ljb@users.noreply.github.com>
2025-11-11 09:14:43 +0100

    first commit

--- src/app.component.ts
+++ src/app.component.ts
@@ -5,6 +5,16 @@ import { RouterModule } from '@angular/router';
     selector: 'app-root',
     standalone: true,
     imports: [RouterModule],
-    template: `<router-outlet></router-outlet>`
+    template: `
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KR1H528VQ8"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-KR1H528VQ8');
+        </script>
+        <router-outlet></router-outlet>`
 })
 export class AppComponent {}
