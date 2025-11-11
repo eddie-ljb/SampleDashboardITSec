21bc5bcc0e410327af2e6ede508216b1197314bb
eddie-ljb <69401607+eddie-ljb@users.noreply.github.com>
2025-11-11 10:38:53 +0100

    docker compose

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,20 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AnalyticsService } from '@/services/analytics.service';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [RouterModule],
     template: `
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KR1H528VQ8"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-KR1H528VQ8');
-        </script>
         <router-outlet></router-outlet>`
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+    constructor(
+        private analyticsService: AnalyticsService
+    ) {}
+
+    ngOnInit() {
+        this.analyticsService.trackEvent("app component loaded", "app component loaded successfully", "load");
+    }
+
+}
